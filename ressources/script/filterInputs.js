cf970bef6fae5ab1f028abae55ce9d0b63d8dcf2
vincent DIAZ <vincentdiaz1990@outlook.fr>
2022-09-17 16:59:06 +0200

    reflexion sur la fonction recursive

--- ressources/script/filterInputs.js
+++ ressources/script/filterInputs.js
@@ -2,10 +2,17 @@
 initialLoadFilter();
 
 
+
         // Création des tags correspondant au clique
 
 let tag = [];
+
 let uniqueValeurTag;
+let uniqueValeurTagSplit = [];
+
+let nvTabIngredient = [];
+let nvTabAppliances = [];
+let nvTabUstensils = [];
 
 let liIngredients = document.querySelectorAll('#ingredients-filter-div .nom-filtre');
 let liAppliances = document.querySelectorAll('#appliance-filter-div .nom-filtre');
@@ -17,57 +24,90 @@ let divTagUl = document.querySelector('.tagUl');
 function ingredientsTagEvent(e){
     
     let variable = e.target.innerText;
-    tag.push(variable);
-
-        //Construction d'un tableau avec des valeur unique
-    uniqueValeurTag = [...new Set(tag)]
-
-    console.log(uniqueValeurTag);
-
+    tag.push('ingredients_' + variable);
+    getType();
+    
     divTagUl.innerHTML = "";
     createTag();
+    filterByTag();
 }
 
 function appliancesTag(e){
 
     let variable = e.target.innerText;
-    tag.push(variable);
-    uniqueValeurTag = [...new Set(tag)]
+    tag.push('appliances_'+variable);
+    
+    getType();
     divTagUl.innerHTML = "";
     createTag();
-
+    filterByTag()
 }
 
 function ustensilsTag(e){
 
     let variable = e.target.innerText;
-    tag.push(variable);
-    uniqueValeurTag = [...new Set(tag)]
+    tag.push('ustensils_'+variable);
+
+    getType();
     divTagUl.innerHTML = "";
     createTag();
-
+    filterByTag()
 }
 
+function getType(){
+        //Construction d'un tableau avec des valeur unique
+    uniqueValeurTag = [...new Set(tag)];
+    uniqueValeurTag.forEach(element => {
+        element = element.split('_');
+        element = element[1]
+        uniqueValeurTagSplit.push(element)
+        uniqueValeurTagSplit = [...new Set(uniqueValeurTagSplit)];
+        //console.log(uniqueValeurTagSplit);
+    }); 
+}
 
 
 function createTag(){
 
-    for(let i = 0; i < uniqueValeurTag.length; i++){
+    let wrapperLiTag;
+    let wrapperLiTagCross;
+    let liTag= []
+
+    for(let i = 0; i < uniqueValeurTagSplit.length; i++){
         //console.log(tag[i]);
-        let wrapperLiTag = document.createElement('li');
+        wrapperLiTag = document.createElement('li');
         wrapperLiTag.classList.add('tagLi');
         
-        wrapperLiTag.innerText = ` ${uniqueValeurTag[i]} `
+        wrapperLiTag.innerText = ` ${uniqueValeurTagSplit[i]} `
 
         divTagUl.appendChild(wrapperLiTag);
 
-        let wrapperLiTagSpan = document.createElement('i');
-        wrapperLiTagSpan.classList.add('fa-solid');
-        wrapperLiTagSpan.classList.add('fa-x');
+        liTag.push(wrapperLiTag);
+        liTag = [...new Set(liTag)];
 
-        wrapperLiTag.appendChild(wrapperLiTagSpan);
+        wrapperLiTagCross = document.createElement('i');
+        wrapperLiTagCross.classList.add('fa-solid');
+        wrapperLiTagCross.classList.add('fa-x');
+
+        wrapperLiTag.appendChild(wrapperLiTagCross);
     }
 
+    liTag.forEach(li => {    
+        li.childNodes[5].addEventListener("click", (e) =>{
+            //console.log(e.path[1].innerText);
+            //console.log(wrapperLiTag);
+            wrapperLiTag.remove(e.target);
+            uniqueValeurTagSplit.forEach(element => {
+                //console.log(element);
+                if(e.path[1].innerText.toLowerCase() == element.toLowerCase()){
+                    console.log('toto')
+                    //uniqueValeurTagSplit.pop(element)
+                } 
+            });
+            
+
+        })  
+    });
 }
 
 
@@ -172,6 +212,8 @@ function filterDataLoadingAppliancesTag(e) {
         filtreAppliance.appendChild(wrapperAppliances);
 }
 
+
+
 function filterDataLoadingUstensilsTag(e) {
     let filterActive = e.target.value.toLowerCase();
 
@@ -213,48 +255,115 @@ function filterDataLoadingUstensilsTag(e) {
 
             // Filtre des cartes par les tag
 
-    // 1 - rentrer les valeurs des tags dans un tableau (déja dans : uniqueValeurTag)
+    // 1 - rentrer les valeurs des tags dans un tableau (déja dans : uniqueValeurTagSplit)
+    // 2 - faire un trie par le type de donnée (ingredient/appareil/ustensils) 2 méthodes :
+        // rajouter le type directement dans la fonction ingredientsTagEvent appliancesTag et ustensilsTag
+        // créer un tableau intermediaire dans lequel on met un correspondance clef valeur puis on le push ce tableau dans le tableau tag et on fait une recherche par correspondance du type
     // 2 - Comparer avec le tableau des recettes (recipes)
             // Si correspondance faire un trie et enregistrer ce trie comme nouvelle valeur de recipes pour la fonction filterDataLoadingCard
     // 3 - Mettre en place une croix pour supprimer un tag
     // 4 - Si clique sur une croix alors supprimer le tag puis supprimer sa valeur dans le tableau des tags puis faire un nouveau trie pour recipes
 
+
+
+
+
+
+
 function filterByTag(){
 
     let globalTags = document.querySelectorAll('#filter-tag .tagLi');
 
-    uniqueValeurTag; 
+    let nvTableau = recipesActif;
 
+    console.log(uniqueValeurTagSplit);
+    
 
- /*   
-    if(uniqueValeurTag.length != 0){
-        for( let i=0; i<uniqueValeurTag.length; i++){
-            for(let j=0; j<recipes.length; j++){
-                if(uniqueValeurTag[i].innerText.toLowerCase() == recipes[j].innerText.toLowerCase()){
-                    recipes = []
-                    recipes.push
-                }
-            }
+    uniqueValeurTag.forEach(tag => {
+
+        let tagWithoutType = tag.split("_");
+        let tagWithoutTypeUnique = [...new Set(tagWithoutType)]
+        console.log(tagWithoutTypeUnique);
+
+        if(tagWithoutTypeUnique[0] == 'ingredients'){           
+            nvTableau = filterByTagIngredient(tagWithoutTypeUnique[1].replace(/[^a-zA-Z0-9]/g, ''), nvTableau);
         }
-  */
-
+        if(tagWithoutTypeUnique[0] == 'appliances'){           
+            nvTableau = filterByTagAppliances(tagWithoutTypeUnique[1].replace(/[^a-zA-Z0-9]/g, ''), nvTableau);
+        }
+        if(tagWithoutTypeUnique[0] == 'ustensils'){           
+            nvTableau = filterByTagUstensils(tagWithoutTypeUnique[1].replace(/[^a-zA-Z0-9]/g, ''), nvTableau);
+        }
+    });
 }
 
 
+function filterByTagIngredient(valeur, nvTableau){
 
 
+    nvTableau.forEach(element => {
+        
+        element.ingredients.forEach(el => {
 
-/*
-function filterTagCardOnLoad(){
+            let ingredient = el.ingredient.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
 
-    const listPlats = document.querySelector('#liste-plats');
+            //console.log(ingredient);
+            //console.log(valeur);
 
-    let filterActive = e.target.value.toLowerCase();
+            if(valeur.toLowerCase() == ingredient){
+                nvTabIngredient.push(element);
+                nvTabIngredient = [...new Set(nvTabIngredient)];
+            }
+        })
+       
+    });
+    //console.log(nvTabIngredient);
 
-    listPlats.innerHTML = "";
-
-    if(globalTags.length != 0){
-
-    }
+    return nvTabIngredient;
 }
-*/
+
+function filterByTagAppliances(valeur, nvTableau){
+
+
+    nvTableau.forEach(element => {
+        
+        let appliance = element.appliance.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+        //console.log(appliance);
+        //console.log(valeur);
+
+        if(valeur.toLowerCase() == appliance){
+            nvTabAppliances.push(element);
+            nvTabAppliances = [...new Set(nvTabAppliances)];
+        }    
+    });
+
+    //console.log(nvTabAppliances);
+
+    return nvTabAppliances;
+}
+
+function filterByTagUstensils(valeur, nvTableau){
+
+
+    nvTableau.forEach(element => {
+        //console.log(element);
+        
+        element.ustensils.forEach(el => {
+
+            let ustensils = el.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+            //console.log(ustensils);
+            //console.log(valeur);
+
+            if(valeur.toLowerCase() == ustensils){
+                nvTabUstensils.push(element);
+                nvTabUstensils = [...new Set(nvTabUstensils)];
+            }
+        })
+       
+    });
+    //console.log(nvTabUstensils);
+
+    return nvTabUstensils;
+}
